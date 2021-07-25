400dfa725cc7d7e07d88f78eda2963374083a00b
lvn3668 <lalitha.viswanathan79@gmail.com>
2021-07-25 02:12:00 -0400

    Initial commit

---
+++ flagstatutilities/flagstatutils.py
@@ -0,0 +1,30 @@
+# Author: Lalitha Viswanathan
+# Flagstat Utils
+############################################################
+from typing import re
+def flagstat(filename: str) -> dict:
+    try:
+        with open(filename) as f:
+            text = f.read()
+        pattern = r'(?P<total_pf>[0-9]+) \+ (?P<total_fail>[0-9]+) in total.*\n' \
+                  + '(?P<duplicates_pf>[0-9]+) \+ (?P<duplicates_fail>[0-9]+) duplicates.*\n' \
+                  + '(?P<mapped_pf>[0-9]+) \+ (?P<mapped_fail>[0-9]+) mapped.*\n' \
+                  + '(?P<paired_pf>[0-9]+) \+ (?P<paired_fail>[0-9]+) paired.*\n' \
+                  + '(?P<read1_pf>[0-9]+) \+ (?P<read1_fail>[0-9]+) read1.*\n' \
+                  + '(?P<read2_pf>[0-9]+) \+ (?P<read2_fail>[0-9]+) read2.*\n' \
+                  + '(?P<properly_paired_pf>[0-9]+) \+ (?P<properly_paired_fail>[0-9]+) properly.*\n' \
+                  + '(?P<self_and_mate_pf>[0-9]+) \+ (?P<self_and_mate_fail>[0-9]+) with itself.*\n' \
+                  + '(?P<singletons_pf>[0-9]+) \+ (?P<singletons_fail>[0-9]+) singletons.*\n' \
+                  + '(?P<different_chr_pf>[0-9]+) \+ (?P<different_chr_fail>[0-9]+) with mate .*\n' \
+                  + '(?P<different_chr_gt5_pf>[0-9]+) \+ (?P<different_chr_gt5_fail>[0-9]+) with mate.*\n'
+        m = re.match(pattern, text)
+        if not m:
+            raise Exception('regex did not match')
+    except (FileNotFoundError, IOError):
+        print("Wrong file or file path")
+
+        if not m.groupdict():
+            raise Exception('Flagstat results not parsed correctly')
+
+    results: dict[str, dict[str, str]] = {'flagstat': m.groupdict()}
+    return results
---
+++ samtoolsparser.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+# Author: Lalitha Viswanathan
+# Samtools parser
+import json
+from argparse import ArgumentParser
+from pprint import pprint
+
+from flagstatutilities.flagstatutils import flagstat
+
+
+def is_json(myjson: json) -> bool:
+    try:
+        json_object = json.loads(myjson)
+    except ValueError as e:
+        print('invalid json: %s' % e)
+        return False
+    return True
+
+
+############################################################
+table_lookup = {
+    'flagstat': 'samtools_flagstat'
+}
+
+
+############################################################
+def parse_samtools_file(samtoolsfilename: str):
+    results: dict = flagstat(samtoolsfilename)
+
+    # Ouptut from samtools ; To be converted to percentage    
+    # {'flagstat': {'singletons_fail': '0', 'self_and_mate_pf': '1040733955', 'singletons_pf': '4163686', 'self_and_mate_fail': '0', 'properly_paired_pf': '984948647', 'total_fail': '0', 'different_chr_gt5_fail': '0', 'different_chr_pf': '18081440', 'duplicates_pf': '16871762', 'read1_pf': '525610111', 'paired_fail': '0', 'read2_pf': '525618053', 'different_chr_fail': '0', 'mapped_fail': '0', 'paired_pf': '1051228164', 'different_chr_gt5_pf': '3935380', 'mapped_pf': '1044897641', 'properly_paired_fail': '0', 'read2_fail': '0', 'duplicates_fail': '0', 'total_pf': '1051228164', 'read1_fail': '0'}} 
+
+    modifiedresults: dict[str, dict] = {'flagstat': {}}
+    # for each key in flagstats, divide it by total_pf 
+    for key in results['flagstat'].keys():
+        modifiedresults['flagstat'][key + "_percent_of_total_pf"] = (float(results['flagstat'][key]) / float(
+            results['flagstat']['total_pf'])) * 100
+    samtoolsparser: dict[str, dict[str, dict]] = {}
+    if modifiedresults:
+        samtoolsparser['modifiedresults'] = modifiedresults
+    else:
+        raise Exception('Modified Flagstat Results not generated correctly')
+
+    if is_json(json.dumps(samtoolsparser)):
+        return json.dumps(samtoolsparser)
+    else:
+        raise Exception('Invalid JSON: Samtools Flagstat')
+
+
+############################################################
+if __name__ == '__main__':
+    parser = ArgumentParser()
+    parser.add_argument('samtoolsfilename')
+    parser.add_argument('sampleid')
+    parser.add_argument('runid')
+    args = parser.parse_args()
+    (results, imagefile) = flagstat(args.samtoolsfilename)
+
+    modifiedresults: dict[str, dict] = {'flagstat': {}}
+    for key in results['flagstat'].keys():
+        modifiedresults['flagstat'][key + "_percent_of_total_pf"] = (float(results['flagstat'][key]) / float(
+            results['flagstat']['total_pf'])) * 100
+    if imagefile:
+        print(imagefile)
+    pprint(modifiedresults)
+
+############################################################
+
+# 4198456 + 0 in total (QC-passed reads + QC-failed reads) 
+# 0 + 0 duplicates 
+# 4022089 + 0 mapped (95.80%:-nan%) 
+# 4198456 + 0 paired in sequencing
+# 2099228 + 0 read1 
+# 2099228 + 0 read2 
+# 3796446 + 0 properly paired (90.42%:-nan%)
+# 4013692 + 0 with itself and mate mapped
+# 8397 + 0 singletons (0.20%:-nan%)
+# 167574 + 0 with mate mapped to a different chr
+# 72008 + 0 with mate mapped to a different chr (mapQ>=5)
